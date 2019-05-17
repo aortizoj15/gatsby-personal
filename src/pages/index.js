1109eede69bc77c519aa990baae2d077ad9cef52
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2019-05-17 11:15:20 -0700

    Created Layout component to avoid rewriting layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Layout from '../components/layout';
 
 const IndexPage = () => (
   <div>
-    <Header />
-    <h1>Hello.</h1>
-    <h2>
-      My name is Alexis Ortiz Ojeda, a full-stack developer from Santa Cruz.
-    </h2>
-    <p>
-      Need a developer? <Link to="/contact">Contact me.</Link>
-    </p>
-    <Footer />
+    <Layout>
+      <h1>Hello.</h1>
+      <h2>
+        My name is Alexis Ortiz Ojeda, a full-stack developer from Santa Cruz.
+      </h2>
+      <p>
+        Need a developer? <Link to="/contact">Contact me.</Link>
+      </p>
+    </Layout>
   </div>
 );
 
