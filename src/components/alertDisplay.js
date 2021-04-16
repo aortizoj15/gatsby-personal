d195ceb5c21eb47f962ac822384c301dc12a36ce
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2021-04-16 01:46:41 -0700

    Fixed broken url pointing to old resume in alert

--- src/components/alertDisplay.js
+++ src/components/alertDisplay.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'gatsby';
 import Alert from '@material-ui/lab/Alert';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
     margin: '1rem 0 2rem',
@@ -16,7 +16,13 @@ function AlertDisplay() {
   return (
     <div className={classes.root}>
       <Alert variant="filled" severity="info">
-        <strong>Need an experienced developer for your team? Hire me! </strong> <a href="https://drive.google.com/open?id=1HoMg9QHGH2qicG8bZ8hRWv7FT7GXjFB5"><strong>Resume</strong></a>{' '}<Link to="/contact"><strong>Contact Information</strong></Link>
+        <strong>Need an experienced developer for your team? Hire me! </strong>{' '}
+        <a href="https://docs.google.com/document/d/19Kjd7Sc2ZaMb9Sk4q2K7_zuZ78vtgmS63pa9xdRu4bA/edit?usp=sharing">
+          <strong>Resume</strong>
+        </a>{' '}
+        <Link to="/contact">
+          <strong>Contact Information</strong>
+        </Link>
       </Alert>
     </div>
   );
